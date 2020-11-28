4d383da896db80029d3d19c749efe670b1f0e6f3
sean <sennis3@uic.edu>
2020-11-28 14:36:31 -0600

    second animations

---
+++ Assets/JeffScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JeffScript : MonoBehaviour
+{
+    //public AudioSource audioSrc;
+    public Animator anima;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //audiosrc.GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider colObj) {
+        if (colObj.gameObject.name == "CameraTrigger") {
+            anima.SetTrigger("JeffTrigger");
+            //audiosrc.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+
+    /*public GameObject seanChar;
+    public GameObject jeffChar;
+    public GameObject anthonyChar;
+    public GameObject otherGuyChar;*/
+    public GameObject girlChar;
+
+    /*public Animator seanAnima;
+    public Animator jeffAnima;
+    public Animator anthonyAnima;
+    public Animator otherGuyAnima;*/
+    public Animator girlAnima;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello");
+        /*var distance1 = Vector3.Distance(transform.position, seanChar.transform.position);
+        var distance2 = Vector3.Distance(transform.position, jeffChar.transform.position);
+        var distance3 = Vector3.Distance(transform.position, anthonyChar.transform.position);
+        var distance4 = Vector3.Distance(transform.position, otherGuyChar.transform.position);*/
+        var distance5 = Vector3.Distance(transform.position, girlChar.transform.position);
+
+        Debug.Log(distance5);
+        if (distance5 < 3) {
+            girlAnima.SetTrigger("New Trigger");
+            Debug.Log("Girl Triggered");
+        }
+    }
+}
