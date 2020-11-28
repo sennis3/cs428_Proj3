3e70c052385f6e8d9ac162bac94d23beb12dac8d
sean <sennis3@uic.edu>
2020-11-28 17:08:17 -0600

    Spawns soda cans from vending machine

---
+++ Assets/Scripts/SpawnCan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCan : MonoBehaviour
+{
+    public Transform startPos;
+    public GameObject sodaCan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //public static Object Instantiate(sodaCan, startPos.position, startPos.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void spawnCan()
+    {
+        Instantiate(sodaCan, startPos.position, startPos.rotation);
+    }
+}
